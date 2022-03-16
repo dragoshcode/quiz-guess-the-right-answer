d60d309d882538adcedb186658f6a540c696bfce
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-03-16 17:25:17 +0200

    Create logic for checking if answer is correct & moving to the next question

--- src/App.tsx
+++ src/App.tsx
@@ -52,9 +52,32 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.KeyboardEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
 
-  const nextQuestion = () => {};
+      if (correct) setScore((prev) => prev + 1);
+
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    const nextQuestion = number + 1;
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div className='App'>
@@ -76,9 +99,14 @@ const App = () => {
           callback={checkAnswer}
         />
       )}
-      <button className='next' onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!gameOver &&
+        !loading &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 && (
+          <button className='next' onClick={nextQuestion}>
+            Next Question
+          </button>
+        )}
     </div>
   );
 };
